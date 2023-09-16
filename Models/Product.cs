7b9a1c8684843f8c1d962c85855158d55e5b4b66
sam <a0983131803@gmail.com>
2023-09-16 15:55:52 +0800

    add User Product models

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+namespace dashboard.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Brand { get; set; }
+
+        public int Price { get; set;}
+
+        public int Quantity { get; set; }
+
+        public DateTime PurchaseDate { get; set; } 
+
+        public DateTime PurchaseTime { get; set; } 
+    }
+    
+}
